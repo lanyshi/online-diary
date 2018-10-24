bb90d05ec4dd5317ece0d9aa7b36eb1e19a2cdee
lanyshi <lanyshi@iu.edu>
2018-10-23 22:08:12 -0400

    Uploading venv Folder

---
+++ dw_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/nfs/nfs7/home/lanyshi/a348-spr2018-workspace/final-project/fp_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
